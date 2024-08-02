7e536cc94ca09964f3ff68102c529e393089726b
Evan-CwC <evancwc0305@gmail.com>
2024-08-02 17:28:17 +1000

    first commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,52 +1,45 @@
 const cardList = [
   {
-    title: "Kitten 2",
-    image: "cat 2.jpeg",
-    link: "About Kitten 2",
-    desciption: "Demo desciption about kitten 2",
+    title: "Scottish Fold",
+    image: "cat 3.jpg",
+    link: "About Scottish Fold ",
+    description:
+      "The Scottish Fold is a distinctive breed of domestic cat characterised by a natural dominant gene mutation associated with osteochondrodysplasia. This genetic anomaly affects cartilage throughout the body, causing the ears to fold, bending forward and down towards the front of the head.",
   },
   {
-    title: "Kitten 3",
-    image: "cat 3.jpg",
-    link: "About Kitten 3",
-    desciption: "Demo desciption about kitten 3",
+    title: "Ragdoll",
+    image: "Ragdoll.jpg",
+    link: "About Ragdoll",
+    description:
+      "The Ragdoll is a breed of cat with a distinct colorpoint coat and blue eyes. Its morphology is large and weighty, and it has a semi-long and silky soft coat. American breeder Ann Baker developed Ragdolls in the 1960s. They are best known for their docile, placid temperament and affectionate nature.",
+  },
+  {
+    title: "British Shorthair",
+    image: "shorthair.jpg",
+    link: "About Shorthair",
+    description:
+      "The British Shorthair is the pedigreed version of the traditional British domestic cat, with a distinctively stocky body, thick coat, and broad face. The most familiar colour variant is the British Blue, with a solid grey-blue coat, pineapple eyes, and a medium-sized tail.",
   },
 ];
 
-const clickMe = () => {
-  alert("Thanks for clicking me. Hope you have a nice day!");
-};
-
-const submitForm = () => {
-  let formData = {};
-  formData.first_name = $("#first_name").val();
-  formData.last_name = $("#last_name").val();
-  formData.password = $("#password").val();
-  formData.email = $("#email").val();
-  console.log("Form Data Submitted: ", formData);
-};
-
 const addCards = (items) => {
   items.forEach((item) => {
-    let itemToAppend =
-      '<div class="col s4 center-align">' +
-      '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
-      item.image +
-      '">' +
-      '</div><div class="card-content">' +
-      '<span class="card-title activator grey-text text-darken-4">' +
-      item.title +
-      '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
-      item.link +
-      "</a></p></div>" +
-      '<div class="card-reveal">' +
-      '<span class="card-title grey-text text-darken-4">' +
-      item.title +
-      '<i class="material-icons right">close</i></span>' +
-      '<p class="card-text">' +
-      item.desciption +
-      "</p>" +
-      "</div></div></div>";
+    let itemToAppend = `
+      <div class="col s12 m6 center-align">
+        <div class="card">
+          <div class="card-image">
+            <img src="${item.image}" />
+            <span class="card-title">${item.title}</span>
+            <a class="btn-floating halfway-fab waves-effect waves-light #0091ea light-blue accent-4">
+              <i class="material-icons">add</i>
+            </a>
+          </div>
+          <div class="card-content">
+            <p>${item.description}</p>
+          </div>
+        </div>
+      </div>
+    `;
     $("#card-section").append(itemToAppend);
   });
 };
@@ -58,4 +51,11 @@ $(document).ready(function () {
   });
   addCards(cardList);
   $(".modal").modal();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".fixed-action-btn");
+  var instances = M.FloatingActionButton.init(elems, {
+    toolbarEnabled: true,
+  });
 });
